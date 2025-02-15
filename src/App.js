fca6e655aa934ccb8740dae503bb28eb08b7151e
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-15 18:59:54 +0400

    Added custome colour for the button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { Box, ThemeProvider } from '@mui/material';
 
 import Exercise from './pages/Exercise';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import theme from './theme';
 
 const App = () => {
   return (
-    <Box width="400px" sx = {{width: {xl : '1488px'}}} m = 'auto'>
+    <ThemeProvider theme = {theme}>
+      <Box width="400px" sx = {{width: {xl : '1488px'}}} m = 'auto'>
         <Navbar />
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/exercise/:id' element = {<Exercise />} />
         </Routes>
         <Footer />
-    </Box>
+      </Box>
+    </ThemeProvider>
   )
 }
 
