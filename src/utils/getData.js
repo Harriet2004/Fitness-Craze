dde6f2a89ccd259c3daf0c562215d8acd48490f9
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-15 21:51:17 +0400

    Incorporated API into the code

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,3 +1,13 @@
-export const getData() = async () => {
-    
+export const exerciseOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+	}
+};
+
+export const getData = async (url, options) => {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
 }
