46c2c560d3dfa2f74407b7db0af634b0bfaf74df
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-15 14:21:19 +0400

    Added content for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Box } from '@mui/material'
+
+import Exercises from '../components/Exercises'
+import Introduction from '../components/Introduction'
+import Search from '../components/Search'
+import Reviews from '../components/Reviews'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <Introduction />
+      <Search />
+      <Exercises />
+      <Reviews />
+    </Box>
   )
 }
 
