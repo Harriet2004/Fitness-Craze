87754646603a62377f04cb87815866f9ce19d5e0
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-16 00:12:13 +0400

    Modified Home.js to display the exercises

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box } from '@mui/material'
 
 import Exercises from '../components/Exercises'
@@ -7,11 +7,13 @@ import Search from '../components/Search'
 import Reviews from '../components/Reviews'
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <Introduction />
-      <Search />
-      <Exercises />
+      <Search setExercises = {setExercises} bodyPart = {bodyPart} setBodyPart = {setBodyPart}/>
+      <Exercises setExercises = {setExercises} bodyPart = {bodyPart} setBodyPart = {setBodyPart}/>
       <Reviews />
     </Box>
   )
