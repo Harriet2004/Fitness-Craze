ad916cf89573f1f50fd12ee815b1f97114e82e6a
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-15 13:46:42 +0400

    Modified the navbar with proper sstlying

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../assets/muscle.png';
+
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack 
+    direction="column"
+    sx = {{mt: {sm: '32px', xs: '20px'}}}
+    justifyContent="center"
+    alignItems="center"
+    width='100%'
+    >
+      <Link to='/'>
+        <img src={Logo} alt = 'Logo' style ={{marginBottom: '10px', width: '175px', height: '60px'}} />
+      </Link>
+        <Stack
+        direction="row"
+        fontSize="24px"
+        gap="40px"
+        alignItems="flex-end"
+        >
+          <Link to ='/' style = {{color: 'black', textDecoration: 'none', borderBottom: '3px solid black'}}>
+            Home
+          </Link>
+          <a href="#exercises" style={{textDecoration: 'none', color: 'black', borderBottom: '3px solid black'}}> Exercises </a>
+          <a href="#reviews" style={{textDecoration: 'none', color: 'black', borderBottom: '3px solid black'}}> Reviews </a>
+
+        </Stack>
+    </Stack>
   )
 }
 
