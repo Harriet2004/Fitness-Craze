13ad4523050ddbcba76aec67b00d00f9415ab6ad
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-17 13:03:51 +0400

    Implemented basic structuer for exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { Pagination } from '@mui/material/Pagination'
+import {Box, Typography, Stack} from '@mui/material'
+import { getData, exerciseOptions } from '../utils/getData'
+import ExerciseCard from './ExerciseCard'
 
-const Exercises = () => {
+const Exercises = ({exercises, setExercises, bodyPart}) => {
   return (
-    <div>Exercises</div>
-  )
-}
+      <Box id="exercises" 
+      mt = '50px'
+      p = '20px'
+      sx = {{mt: {lg: '110px'}}}
+      >
+        <Typography variant='h2' mb='40px'>
+          Showing results.
+        </Typography>
+        <Stack direction='row' justifyContent='center' flexWrap='wrap' sx = {{gap: {lg: '110px', xs: '50px'}}}>
+          {exercises.map((exercise,index) => (
+            <ExerciseCard key={index} exercise = {exercise}/>
+          ))}
+        </Stack>
+      </Box>
+    )
+  }
 
 export default Exercises
