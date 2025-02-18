a78f9afe12d06cbd5fc7136de5ceacbc39a6b2ff
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 12:20:59 +0400

    Debugged search bar issues

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,29 +10,40 @@ const Search = ({setExercises, setBodyPart, bodyPart}) => {
   useEffect(() => {
     const getExerciseData = async () => {
       const bodyPartsData = await getData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      console.log('Body Parts Data:', bodyPartsData);
       setBodyParts(['all', ...bodyPartsData]);
     }
     getExerciseData();
   }, [])
 
   const searchQuery = async () => {
-    if(search) {
-      const data = await getData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
-      
+  if (search) {
+    // Construct URL based on search term
+    const url = `https://exercisedb.p.rapidapi.com/exercises/name/${search}?offset=0&limit=1000`;
+
+    const data = await getData(url, exerciseOptions);
+
+    if (Array.isArray(data)) {
       const searchExercise = data.filter(
-        (exercise) => exercise.name.toLowerCase().includes(search)
-        || exercise.target.toLowerCase().includes(search)
-        || exercise.bodyPart.toLowerCase().includes(search)
-        || exercise.equipment.toLowerCase().includes(search)
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search)
       );
+      window.scrollTo({ top: 1275, left: 100, behavior: 'smooth' });
       setSearch('');
-      setExercises([searchExercise]);
+      setExercises(searchExercise); 
+    } else {
+      console.error("Expected an array, but got:", data);
     }
   }
+};
+
 
   return (
-    <Stack justifyContent='center' alignItems='center' mt='40px' p='20px'>
-      <Typography fontWeight='700' textAlign='center' sx={{fontSize: {lg: '40px', xs: '30px'}}} mb='50px'>
+    <Stack justifyContent='center' alignItems='center' mt='10px' p='20px'>
+      <Typography fontWeight='700' textAlign='center' sx={{fontSize: {lg: '40px', xs: '30px'}}} mb='10px'>
         Check out some cool exercises!
       </Typography>
       <Box mb='70px' position = 'relative'>
@@ -46,7 +57,6 @@ const Search = ({setExercises, setBodyPart, bodyPart}) => {
             width: {lg: '800px', xs: '400px'},
             backgroundColor: 'white',
             input: {border: 'none', fontWeight: '700'},
-            borderRadius: '40px'
           }}
         />
         <Button className='search-btn'
@@ -63,7 +73,7 @@ const Search = ({setExercises, setBodyPart, bodyPart}) => {
           onClick={searchQuery}    
         >SEARCH</Button>
       </Box>
-      <Box sx={{position: 'relative', width: '100%', p: '15px'}}>
+      <Box sx={{position: 'relative', width: '100%', p: '25px'}}>
         <ScrollBar data = {bodyParts} bodyPart = {bodyPart} setBodyPart = {setBodyPart}/>
       </Box>
     </Stack>
