c09869783c3c449a11eac0adb0aee843c985418c
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 12:19:52 +0400

    Made the exercise cards once searched

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Stack, Typography } from '@mui/material'
 
-const ExerciseCard = () => {
+const ExerciseCard = ({ exercise }) => {
   return (
-    <div>ExerciseCard</div>
+    <Link className='exercise-card' to={`/exercise/${exercise.id}`}>
+        <img src={exercise.gifUrl} alt={exercise.name} loading='lazy' />
+        <Stack direction='row'>
+            <Button sx={{fontSize:'14px',textTransform:'capitalize',ml:'20px',color:'black',backgroundColor:'#ffbd59'}}>
+                {exercise.bodyPart}
+            </Button>
+            <Button sx={{fontSize:'14px',textTransform:'capitalize',ml:'20px',color:'black',backgroundColor:'#ffbd59'}}>
+                {exercise.bodyPart}
+            </Button>
+        </Stack>
+        <Typography ml="20px" color="black" textTransform="capitalize" fontSize="20px" fontWeight="bold" pb="10px" mt="10px">
+            {exercise.name}
+        </Typography>
+    </Link>
   )
 }
 
