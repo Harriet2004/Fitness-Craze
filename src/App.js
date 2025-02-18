915f29c06a79fee2e2e6ba82ffe9d5d3300fd1b3
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 12:19:16 +0400

    Removed Navbar from App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const App = () => {
   return (
     <ThemeProvider theme = {theme}>
       <Box width="400px" sx = {{width: {xl : '1488px'}}} m = 'auto'>
-        <Navbar />
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/exercise/:id' element = {<Exercise />} />
