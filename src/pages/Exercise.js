23e27c9473bf609b6f733f097f931e2ba2f2862c
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 12:42:44 +0400

    Started working on the page for exercises

--- src/pages/Exercise.js
+++ src/pages/Exercise.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import ExerciseDetails from '../components/ExerciseDetails'
+import Videos from '../components/Videos'
+import SimilarExercise from '../components/SimilarExercise'
+
+import { getData, exerciseOptions } from '../utils/getData'
 
 const Exercise = () => {
+  const [exerciseDetail, setExerciseDetail] = useState([]);
+  useEffect(() => {
+    const getExerciseData = async (params) => {
+      const exerciseUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+      const data = await getData(`${exerciseUrl}/exercises/${id}`, exerciseOptions);
+      setExerciseDetail(data);
+    }
+    getExerciseData();
+  }, [id])
+  const {id} = useParams();
   return (
-    <div>Exercise</div>
+    <Box>
+      <ExerciseDetails exerciseDetail = {exerciseDetail} />
+      <Videos/>
+      <SimilarExercise/>
+    </Box>
   )
 }
 
