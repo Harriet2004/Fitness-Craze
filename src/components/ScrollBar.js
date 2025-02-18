6b396f4893a20395ca4dd6fa919c9eed647e3532
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 16:54:06 +0400

    Modified Scroll bar to use in exercise page too

--- src/components/ScrollBar.js
+++ src/components/ScrollBar.js
@@ -3,6 +3,7 @@ import { ScrollMenu, VisibilityContext }  from 'react-horizontal-scrolling-menu'
 import { Box, Typography } from '@mui/material'
 import BodyPart from './BodyPart'
 import { useContext } from 'react'
+import ExerciseCard from '../components/ExerciseCard'
 import Left from '../assets/left.png'
 import Right from '../assets/right.png'
 
@@ -26,12 +27,15 @@ const RightArrow = () => {
   );
 };
 
-const ScrollBar = ({data, bodyPart, setBodyPart}) => {
+const ScrollBar = ({data, bodyPart, isBodyPart, setBodyPart}) => {
+  if (!Array.isArray(data)) {
+    return <Typography>No data available</Typography>;
+  }
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => (
         <Box key={item.id || item} itemId={item.id || item} title = {item.id||item} m = "0 40px">
-          <BodyPart item = {item} bodyPart = {bodyPart} setBodyPart = {setBodyPart} />
+          {isBodyPart ? <BodyPart item = {item} bodyPart = {bodyPart} setBodyPart = {setBodyPart} /> : <ExerciseCard exercise={item} />}
         </Box>
       )
       )}
