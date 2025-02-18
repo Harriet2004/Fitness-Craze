8ca9abfa6731b31eb58317a0df22620bcc06b96a
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 16:54:33 +0400

    Implemented similar exercises component

--- src/components/SimilarExercise.js
+++ src/components/SimilarExercise.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import ScrollBar from '../components/ScrollBar'
+import Loader from '../components/Loader'
 
-const SimilarExercise = () => {
+const SimilarExercise = ({exerciseTarget, exerciseEquipment}) => {
   return (
-    <div>SimilarExercise</div>
+    <Box sx={{ mt: { lg: '70px', xs: '0px' } }}>
+        <Typography sx={{ fontSize: { lg: '40px', xs: '25px' } }} fontWeight={500} color="#000" mb="30px" textAlign='center'>
+          Watch more videos for similar <span style = {{color:'rgb(227, 147, 19)', textTransform:'uppercase', fontWeight:'bold'}}> target muscles </span>
+        </Typography>
+        <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+          {exerciseTarget.length !== 0 ? <ScrollBar data={exerciseTarget} /> : <Loader />}
+        </Stack>
+        <Typography sx={{ fontSize: { lg: '40px', xs: '25px' }, mt: { lg: '100px', xs: '60px' } }} fontWeight={500} color="#000" mb="30px" textAlign="center">
+          Watch more videos for similar <span style = {{color:'rgb(227, 147, 19)', textTransform:'uppercase', fontWeight:'bold'}}> equipment exercises </span>
+        </Typography>
+        <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+          {exerciseEquipment.length !== 0 ? <ScrollBar data={exerciseEquipment} /> : <Loader />}
+        </Stack>
+    </Box>
   )
 }
 
