f8f43d8ba9341a2bc9bfbe4927d084ab11bafe4b
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 16:54:58 +0400

    Added the API calls for the similar exercises in the main file

--- src/pages/Exercise.js
+++ src/pages/Exercise.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
+import Navbar from '../components/ExerciseNav'
 import ExerciseDetails from '../components/ExerciseDetails'
 import Videos from '../components/Videos'
 import SimilarExercise from '../components/SimilarExercise'
@@ -11,6 +12,8 @@ import { getData, exerciseOptions, youtubeOptions } from '../utils/getData'
 const Exercise = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideo, setExerciseVideo] = useState([]);
+  const [exerciseEquipment, setExerciseEquipment] = useState([]);
+  const [exerciseTarget, setExerciseTarget] = useState([]);
   const {id} = useParams();
   useEffect(() => {
     const getExerciseData = async (params) => {
@@ -21,14 +24,21 @@ const Exercise = () => {
 
       const youtubeData = await getData(`${youtubeUrl}/search?query=${detailData.name}`,youtubeOptions);
       setExerciseVideo(youtubeData.contents);
+
+      const targetData = await getData(`${exerciseUrl}/exercises/target/${detailData.target}`, exerciseOptions);
+      setExerciseTarget(targetData);
+
+      const equipmentData = await getData(`${exerciseUrl}/exercises/equipment/${detailData.equipment}`, exerciseOptions);
+      setExerciseEquipment(equipmentData);
     }
     getExerciseData();
   }, [id])
   return (
     <Box>
+      <Navbar />
       <ExerciseDetails exerciseDetail = {exerciseDetail} />
       <Videos exerciseVideo = {exerciseVideo} name = {exerciseDetail.name}/>
-      <SimilarExercise/>
+      <SimilarExercise exerciseTarget = {exerciseTarget} exerciseEquipment = {exerciseEquipment} />
     </Box>
   )
 }
