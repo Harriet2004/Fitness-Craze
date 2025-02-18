c4338231fb8ba411263a46106a6d50fc99fe76b1
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 12:19:32 +0400

    Made the bg image of home page display a bit better

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,44 @@
-import React, { useState } from 'react'
-import { Box } from '@mui/material'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
-import Exercises from '../components/Exercises'
-import Introduction from '../components/Introduction'
-import Search from '../components/Search'
-import Reviews from '../components/Reviews'
+import Exercises from '../components/Exercises';
+import Introduction from '../components/Introduction';
+import Search from '../components/Search';
+import Reviews from '../components/Reviews';
+import Navbar from '../components/Navbar'; 
+import Bg from '../assets/new-bg.png';
 
 const Home = () => {
   const [bodyPart, setBodyPart] = useState('all');
   const [exercises, setExercises] = useState([]);
+
   return (
     <Box>
-      <Introduction />
-      <Search setExercises = {setExercises} bodyPart = {bodyPart} setBodyPart = {setBodyPart}/>
-      <Exercises setExercises = {setExercises} bodyPart = {bodyPart} exercises = {exercises}/>
-      <Reviews />
-    </Box>
-  )
-}
+      <Box
+        sx={{
+          width: "100vw",
+          minHeight: "90vh", 
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${Bg})`, 
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Navbar />
+        <Introduction />
+      </Box>
 
-export default Home
+      <Box sx={{ padding: "20px" }}>
+        <Search setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        <Exercises setExercises={setExercises} bodyPart={bodyPart} exercises={exercises} />
+        <Reviews />
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
