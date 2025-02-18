029ceb4d5f004f45bb8e10664f8f6567c84f9381
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 20:34:26 +0400

    Corrected arrow situation for reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,38 +1,55 @@
-import React from 'react';
-import { useContext } from 'react';
-import { Box, Typography, Stack } from '@mui/material';
+import React, { useContext } from 'react';
+import { Box, Typography } from '@mui/material';
 import { ScrollMenu } from 'react-horizontal-scrolling-menu';
 import { VisibilityContext } from 'react-horizontal-scrolling-menu';
 import Loader from './Loader';
 import Rating from '@mui/material/Rating';
-import Left from '../assets/left.png'
-import Right from '../assets/right.png'
+import Left from '../assets/left.png';
+import Right from '../assets/right.png';
 
+// Left arrow button
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollPrev()} className="left-arrow">
+    <Typography
+      onClick={() => scrollPrev()}
+      className="left-arrow"
+      sx={{
+        position: 'absolute',
+        left: '10px',
+        zIndex: 1,
+      }}
+    >
       <img src={Left} alt="left-arrow" />
     </Typography>
   );
 };
 
+// Right arrow button
 const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollNext()} className="right-arrow">
+    <Typography
+      onClick={() => scrollNext()}
+      className="right-arrow"
+      sx={{
+        position: 'absolute',
+        right: '10px',
+        zIndex: 1,
+      }}
+    >
       <img src={Right} alt="right-arrow" />
     </Typography>
   );
 };
 
 const ReviewCard = ({ review }) => (
-  <Box bgcolor="#f5f5f5" p={2} m={2} borderRadius="10px" width="300px" sx={{overflow: 'hidden', wordWrap: 'break-word', textOverflow: 'ellipsis'}} >
+  <Box bgcolor="#f5f5f5" p={2} m={2} borderRadius="10px" width="300px" sx={{ overflow: 'hidden', wordWrap: 'break-word', textOverflow: 'ellipsis' }}>
     <Typography variant="h5" fontWeight="bold" textAlign="center" mb="20px">{review.name}</Typography>
     <Typography variant="h7">"{review.comment}"</Typography>
-    <br/> <br/>
+    <br /> <br />
     <Rating defaultValue={review.rating} precision={0.5} readOnly />
   </Box>
 );
@@ -41,11 +58,11 @@ const ReviewCarousel = ({ reviews }) => {
   if (!reviews || reviews.length === 0) return <Loader />;
 
   return (
-    <Box>
-      <Typography variant='h1' mb='40px' sx={{ fontSize: {lg:'40px',xs:'30px'}}} fontWeight='bold' textAlign='center' mt = '40px'>
+    <Box id = "reviews" sx={{ position: 'relative', pb: '20px', m: '0 43px' }}> 
+      <Typography variant='h1' mb='40px' sx={{ fontSize: { lg: '40px', xs: '30px' } }} fontWeight='bold' textAlign='center' mt='40px'>
         CUSTOMER REVIEWS
       </Typography>
-      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      <ScrollMenu LeftArrow={<LeftArrow/>} RightArrow={<RightArrow/>}>
         {reviews.map((review, index) => (
           <Box key={index} itemId={index}>
             <ReviewCard review={review} />
@@ -57,3 +74,4 @@ const ReviewCarousel = ({ reviews }) => {
 };
 
 export default ReviewCarousel;
+
