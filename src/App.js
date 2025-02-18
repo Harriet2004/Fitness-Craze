9e4c69e8b9f4771615e1111be4a0918ae95a7ed5
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-18 20:33:13 +0400

    Added scrollToTop to exercises details

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Exercise from './pages/Exercise';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import theme from './theme';
 
 const App = () => {
   return (
     <ThemeProvider theme = {theme}>
       <Box width="400px" sx = {{width: {xl : '1488px'}}} m = 'auto'>
+      <ScrollToTop />
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/exercise/:id' element = {<Exercise />} />
